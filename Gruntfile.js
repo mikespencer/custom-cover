e27d38a3f86d54f577ea62f35bbde2911200eb65
mikespencer <michael.spencer@washingtonpost.com>
2013-09-13 14:35:30 -0400

    updates

---
+++ Gruntfile.js
@@ -0,0 +1,303 @@
+var LIVERELOAD_PORT = 35729;
+var lrSnippet = require('connect-livereload')({port: LIVERELOAD_PORT});
+var mountFolder = function (connect, dir) {
+  return connect.static(require('path').resolve(dir));
+};
+
+var cheerio = require('cheerio');
+var sys = require('sys');
+var exec = require('child_process').exec;
+
+module.exports = function (grunt) {
+  // load all grunt tasks
+  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+  var yeomanConfig = {
+    app: 'src',
+    dist: 'dist'
+  };
+
+  grunt.initConfig({
+    yeoman: yeomanConfig,
+    prodPath: 'wp-srv/ad/public/custom-cover/dist',
+    pkg: grunt.file.readJSON('package.json'),
+    animSpeed: 1000,
+    clean: {
+      dist: {
+        files: [{
+          dot: true,
+          src: [
+            '<%= yeoman.dist %>/*',
+            '!<%= yeoman.dist %>/.git*'
+          ]
+        }]
+      },
+      temp: {
+        files: [{
+          dot: true,
+          src: [
+            '.tmp'
+          ]
+        }]
+      }
+    },
+    useminPrepare: {
+      options: {
+        dest: '<%= yeoman.dist %>'
+      },
+      html: '<%= yeoman.app %>/index.html'
+    },
+    usemin: {
+      options: {
+        dirs: ['<%= yeoman.dist %>']
+      },
+      html: ['<%= yeoman.dist %>/{,*/}*.html'],
+      css: ['<%= yeoman.dist %>/css/{,*/}*.css']
+    },
+    copy: {
+      dist: {
+        files: [{
+          expand: true,
+          dot: true,
+          cwd: '<%= yeoman.app %>',
+          dest: '<%= yeoman.dist %>',
+          src: [
+            '*.{ico,png,txt}',
+            '.htaccess',
+            'img/{,*/}*.{webp,gif}',
+            'css/fonts/*'
+          ]
+        }]
+      },
+      styles: {
+        expand: true,
+        dot: true,
+        cwd: '<%= yeoman.app %>/css',
+        dest: '<%= yeoman.dist %>/css',
+        src: '**/*.css'
+      }
+    },
+    concat: {
+      dist: {
+        options: {
+          banner: '<!-- AD ID: %eaid! -->' +
+            ('' ? '\n<!-- BUG ID:  -->' : '') +
+            '\n<!-- custom-cover-template - 718x512 -->' +
+            '\n\n',
+          process: {
+            data: {
+              clickTracker: '%%CLICK_URL_UNESC%%',
+              clickTrackerEsc: '%%CLICK_URL_ESC%%',
+              clickTag: '%%DEST_URL%%',
+              js: 'http://js.washingtonpost.com/<%= prodPath %>/js/main.min.js',
+              css: 'http://css.wpdigital.net/<%= prodPath %>/css/style.min.css'
+            }
+          }
+        },
+        files: {
+          '<%= yeoman.dist %>/dfp.html': '<%= yeoman.dist %>/index.html'
+        }
+      },
+      dev: {
+        options: {
+          banner: '<!DOCTYPE html>\n<html>\n<head>\n  <title><%= pkg.name %>: Test Page</title>\n</head>\n<body>\n\n',
+          footer: '\n\n</body>\n</html>',
+          process: {
+            data: {
+              clickTracker: '',
+              clickTrackerEsc: '',
+              clickTag: 'http://www.example.com',
+              js: 'js/main.min.js',
+              css: 'css/style.min.css'
+            }
+          }
+        },
+        files: {
+          '<%= yeoman.dist %>/index.html': '<%= yeoman.dist %>/index.html'
+        }
+      }
+    },
+    htmlmin: {
+      dist: {
+        files: [{
+          expand: true,
+          cwd: '<%= yeoman.app %>',
+          src: '*.html',
+          dest: '<%= yeoman.dist %>'
+        }]
+      }
+    },
+    cssmin: {
+      options: {
+        banner: '/* <%= grunt.template.today("yyyy-mm-dd") %> | WP ADOPS */'
+      },
+      dist: {
+        files: {
+          '<%= yeoman.dist %>/css/style.min.css': ['<%= yeoman.app %>/css/style.css']
+        }
+      }
+    },
+    jshint: {
+      gruntfile: {
+        src: 'Gruntfile.js'
+      },
+      src: {
+        src: ['<%= yeoman.app %>/js/*.js']
+      }
+    },
+    uglify: {
+      options: {
+        banner: '/* <%= grunt.template.today("yyyy-mm-dd") %> | WP ADOPS */\n',
+        sourceMap: '<%= yeoman.dist %>/js/main.map.js',
+        sourceMapRoot: '../../',
+        sourceMappingURL: 'js/main.map.js'
+      },
+      main: {
+        src: '<%= yeoman.app %>/js/main.js',
+        dest: '<%= yeoman.dist %>/js/main.min.js'
+      }
+    },
+    compass: {
+      dist: {
+        options: {
+          config: 'config.rb'
+        }
+      }
+    },
+    watch: {
+      options: {
+        nospawn: true
+      },
+      build_html: {
+        files: ['<%= yeoman.app %>/**/*.html'],
+        tasks: ['build']
+      },
+      build_js: {
+        files: ['<%= yeoman.app %>/js/**/*.js'],
+        tasks: ['build']
+      },
+      build_css: {
+        files: ['<%= yeoman.app %>/sass/**/*.sass'],
+        tasks: ['build']
+      },
+      tests: {
+        files: ['test/**/*'],
+        tasks: ['test']
+      },
+      livereload: {
+        options: {
+          livereload: LIVERELOAD_PORT
+        },
+        files: [
+          '<%= yeoman.dist %>/**/*'
+        ]
+      }
+    },
+    connect: {
+      options: {
+        port: 5000,
+        // change this to '0.0.0.0' to access the server from outside
+        hostname: 'localhost'
+      },
+      livereload: {
+        options: {
+          middleware: function (connect) {
+            return [
+              lrSnippet,
+              mountFolder(connect, '.')
+            ];
+          }
+        }
+      }
+    },
+    open: {
+      server: {
+        path: 'http://localhost:<%= connect.options.port %>/dist/'
+      }
+    },
+    qunit: {
+      all: ['test/**/*.html']
+    },
+    absolute: {
+      dist: {
+        src: '<%= yeoman.dist %>/dfp.html',
+        path: '<%= prodPath %>',
+        www: 'http://www.washingtonpost.com',
+        css: 'http://css.washingtonpost.com',
+        img: 'http://img.wpdigital.net',
+        js: 'http://js.washingtonpost.com'
+      }
+    }
+  });
+
+  grunt.registerMultiTask('absolute', 'Make URLs absolute', function(){
+
+    console.log('Transforming relative URL\'s --> absolute URL\'s for: ' + this.data.src);
+
+    var data = this.data;
+    var src = data.src;
+    var defaultBase = 'http://www.washingtonpost.com';
+    var path = data.path.replace(/^\//, '').replace(/\/$/, '');
+    var urls = {
+      www: (data.www ? data.www.replace(/\/$/, '') : defaultBase) + '/' + path,
+      img: (data.img ? data.img.replace(/\/$/, '') : defaultBase) + '/' + path,
+      js: (data.js ? data.js.replace(/\/$/, '') : defaultBase) + '/' + path,
+      css: (data.css ? data.css.replace(/\/$/, '') : defaultBase) + '/' + path
+    };
+
+    var $ = cheerio.load(grunt.file.read(data.src));
+
+    $('script').each(function(){
+      var src = $(this).attr('src'), newSrc;
+      if(src && !/^http|^\/\/\:/.test(src)){
+        newSrc = urls.js + '/' + src.replace(/^\//, '');
+        $(this).attr('src', newSrc);
+      }
+    });
+
+    $('img').each(function(){
+      var src = $(this).attr('src'), newSrc;
+      if(src && !/^http|^\/\/\:/.test(src)){
+        newSrc = urls.img + '/' + src.replace(/^\//, '');
+        $(this).attr('src', newSrc);
+      }
+    });
+
+    $('link').each(function(){
+      var href = $(this).attr('href'), newHref;
+      if(href && !/^http|^\/\/\:/.test(href)){
+        newHref = urls.css + '/' + href.replace(/^\//, '');
+        $(this).attr('href', newHref);
+      }
+    });
+
+    $('.dev-element').remove();
+
+    grunt.file.write(data.src, $.html());
+  });
+
+  grunt.registerTask('default', ['jshint:src', 'build', 'server']);
+
+  grunt.registerTask('test', ['qunit']);
+
+  grunt.registerTask('build', [
+    'jshint:src',
+    'clean:dist',
+    'compass',
+    'concat',
+    'uglify',
+    'cssmin',
+    'htmlmin',
+    'copy:dist',
+    'concat:dist',
+    'concat:dev',
+    'absolute:dist'
+  ]);
+
+  grunt.registerTask('server', [
+    'connect:livereload',
+    'open',
+    'watch'
+  ]);
+
+};
---
+++ src/js/main.js
@@ -0,0 +1,185 @@
+var wpAd = wpAd || {};
+wpAd.CustomCover = (function($){
+
+  'use strict';
+
+  // no console safety fallback for console.log:
+  var console = window.console || { log: function(){} };
+
+  //match this to transition speed in CSS:
+  var animSpeed = 800;
+
+  if(!$){
+    return function(){
+      console.log('Custom Cover failed - jQuery is undefined');
+    };
+  }
+
+  function CustomCover(config){
+    this.config = $.extend({
+
+      creative: '',
+      creativeType: 'image',
+      width: '718px',
+      height: '512px',
+      target: 'article.main:first div.row:first',
+
+      replayCreative: '',
+      replayWidth: '300px',
+      replayHeight: '16px',
+      replayTarget: 'div.customcover-replay-target',
+      prependReplayCreativeToTarget: true,
+
+      clickTracker: '',
+      clickTrackerEsc: '',
+      clickTag: '',
+
+      onInteractionConfig: {
+        // Any values that are passed in to config can be passed in here to tweak the execution to
+        // display a different creative on replay/manual.
+        // eg: video ads - auto is on mute, shorter. manual is full length with sound (change creative)
+        // creative: 'url/to/full/length/video/creative.swf'
+      },
+
+      timeOpen: 7000,
+      auto: true,
+      expDelay: 1000,
+      adid: false
+
+    }, config);
+
+    this.$target = $(this.config.target);
+    this.init();
+  }
+
+  CustomCover.prototype = {
+
+    init: function(){
+      if(this.config.auto){
+        this.exec();
+      } else {
+        this.buildReplayCreative();
+        this.placeReplayCreative();
+      }
+    },
+
+    exec: function(){
+      var root = this;
+      this.buildCreativeContainer();
+      this.buildCreative();
+      this.placeCreative();
+      if(this.config.auto){
+        this.config.auto = false;
+        this.expDelay = setTimeout(function(){
+          root.expand();
+          root.colDelay = setTimeout(function(){
+            root.collapse();
+          }, root.config.timeOpen + animSpeed); // + 1000 is animation speed to fully open.
+        }, this.config.expDelay);
+      } else {
+        this.expDelay = setTimeout(function(){
+          root.expand();
+        }, 100);
+      }
+    },
+
+    buildCreativeContainer: function(){
+      this.$creativeContainer = $('<div class="customcover"></div>').addClass('collapse');
+    },
+
+    buildCreative: function(){
+      var builders = {
+        'image': 'buildImageCreative',
+        'flash': 'buildFlashCreative',
+        'iframe': 'buildIframeCreative',
+        'video': 'buildVideoCreative'
+      };
+      this.config.creativeType = builders[this.config.creativeType] ? this.config.creativeType : 'image';
+      this.creativeCode = this[builders[this.config.creativeType]].call(this);
+      this.$creativeContainer.append(this.creativeCode);
+      this.addCloseButton();
+    },
+
+    buildImageCreative: function(){
+      return '<a href="' + this.config.clickTracker + this.config.clickTag + '" target="_blank">' +
+        '<img src="' + this.config.creative + '" alt="Click here for more information.">' +
+      '</a>';
+    },
+
+    buildFlashCreative: function(){
+      return '';
+    },
+
+    buildIframeCreative: function(){
+      return '';
+    },
+
+    buildVideoCreative: function(){
+      return '';
+    },
+
+    placeCreative: function(){
+      this.$target.prepend(this.$creativeContainer);
+    },
+
+    addReplayButton: function(){
+      var root = this,
+        fn = this.config.prependReplayCreativeToTarget ? 'prependTo' : 'appendTo';
+
+      this.$replayButton = $('<img />').attr({
+        src: this.config.replayCreative,
+        width: this.config.replayWidth,
+        height: this.config.replayHeight,
+        alt: 'Click here to replay advertisement.'
+      }).addClass('replay-customcover').hide()[fn](this.config.replayTarget).on('click', function(){
+        $(this).off('click');
+        root.reconfig(root.config.onInteractionConfig);
+        root.exec();
+      }).show(250);
+    },
+
+    removeReplayButton: function(){
+      if(this.$replayButton){
+        this.$replayButton.remove();
+      }
+    },
+
+    addCloseButton: function(){
+      var root = this;
+      this.closeBtn = $('<span>close [x]</span>').addClass('customcover-closebtn').prependTo(this.$creativeContainer)
+      .on('click', function(){
+        $(this).off('click');
+        root.collapse();
+      });
+    },
+
+    expand: function(){
+      this.removeReplayButton();
+      this.$creativeContainer.addClass('expand').removeClass('collapse');
+    },
+
+    collapse: function(){
+      var root = this;
+      if(this.colDelay){
+        clearTimeout(this.colDelay);
+      }
+      this.$creativeContainer.addClass('collapse').removeClass('expand');
+      setTimeout(function(){
+        root.remove();
+        root.addReplayButton();
+      }, animSpeed);
+    },
+
+    remove: function(){
+      this.$creativeContainer.remove();
+    },
+
+    reconfig: function(data){
+      this.config = $.extend(this.config, data);
+    }
+
+  };
+
+  return CustomCover;
+
+})(window.jQuery);
